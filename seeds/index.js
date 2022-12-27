edc168026e79307da3b44a1f5204ad060f265775
Sami <3004093@edison.k12.nj.us>
2022-12-27 18:14:33 -0500

    Changed up the seeds files to work with the right database

--- seeds/index.js
+++ seeds/index.js
@@ -9,7 +9,10 @@ const Campground = require("../models/campground");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+// const dbUrl =   'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
